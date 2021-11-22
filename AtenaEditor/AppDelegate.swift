8dd94f4febccdebbe1d61f4f4230b6d562893525
vascarpenter <gikoha5666@gmail.com>
2021-11-22 19:46:05 +0900

    Initial Commit

---
+++ AtenaEditor/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  AtenaEditor
+//
+//  Created by Namikare Gikoha on 2021/11/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
